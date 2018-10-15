9105b574fe2d5989898e29d80588f5c11ef82247
Will Adams <bushblade@gmail.com>
2018-10-15 15:07:41 +0100

    animation end instead of settimeout

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ const getImages = async url => {
   return data
 }
 
-getImages('images.json').then(i => {
-  images = i
-  flexContainer.innerHTML = i.reduce((str, x) => str += `<img src="${x}"/>`, '')
-  Array.from(document.querySelectorAll('img')).forEach(x => x.addEventListener('click', modalToggle))
-}).catch(err => console.log(err))
+getImages('images.json')
+  .then(i => {
+    images = i
+    flexContainer.innerHTML = i.reduce((str, x) => (str += `<img src="${x}"/>`), '')
+    Array.from(document.querySelectorAll('img')).forEach(x =>
+      x.addEventListener('click', modalToggle)
+    )
+  })
+  .catch(err => console.log(err))
 
-modal.addEventListener('click', x => x.target.classList.contains('my-modal-content') ? closeModal() : false)
+modal.addEventListener(
+  'click',
+  x => (x.target.classList.contains('my-modal-content') ? closeModal() : false)
+)
 next.addEventListener('click', () => changeImg(+1))
 prev.addEventListener('click', () => changeImg(-1))
 document.getElementById('close').addEventListener('click', closeModal)
@@ -25,16 +32,14 @@ document.addEventListener('keydown', e => {
   if (modal.style.display === 'block') {
     if (e.keyCode === 27) {
       closeModal()
-    }
-    else if (e.keyCode === 37) {
+    } else if (e.keyCode === 37) {
       changeImg(-1)
       prev.focus()
-      setTimeout(()=> prev.blur(),300)
-    }
-    else if (e.keyCode === 39) {
+      setTimeout(() => prev.blur(), 300)
+    } else if (e.keyCode === 39) {
       next.focus()
       changeImg(+1)
-      setTimeout(()=> next.blur(),300)
+      setTimeout(() => next.blur(), 300)
     }
   }
 })
@@ -44,36 +49,37 @@ const changeImg = val => {
     currentIndx = images.indexOf(modalImage.src)
   const switchImage = nextImg => {
     modalImage.classList.add('animated', 'fadeOut')
-    setTimeout(() => {
+    modalImage.addEventListener('animationend', () => {
       modalImage.classList.remove('animated', 'fadeOut')
       modalImage.src = nextImg
       modalImage.classList.add('animated', 'fadeIn')
-    }, 500)
+    })
   }
   if (currentIndx === 0 && val === -1) {
     switchImage(images[images.length - 1])
-  }
-  else if (currentIndx === images.length - 1 && val === +1) {
+  } else if (currentIndx === images.length - 1 && val === +1) {
     switchImage(images[0])
-  }
-  else {
+  } else {
     switchImage(images[currentIndx + val])
   }
 }
+modal.addEventListener('animationend', () => {
+  modal.classList.remove('animated', 'fadeIn', 'fadeOut')
+})
 
 function closeModal() {
+  const animEnd = () => {
+    modal.style.display = 'none'
+    modal.removeEventListener('animationend', animEnd)
+  }
+  modal.addEventListener('animationend', animEnd)
   modal.classList.add('animated', 'fadeOut')
   document.body.style.overflow = ''
-  setTimeout(() => {
-    modal.classList.remove('animated', 'fadeOut')
-    modal.style.display = 'none'
-  }, 500)
 }
 
 function modalToggle(e) {
-  modal.classList.add('animated', 'fadeIn')
   modal.style.display = 'block'
+  modal.classList.add('animated', 'fadeIn')
   modalContent.innerHTML = `<img src="${e.target.src}" id="modal-image">`
   document.body.style.overflow = 'hidden'
-  setTimeout(() => modal.classList.remove('animated', 'fadeIn'), 400)
 }
